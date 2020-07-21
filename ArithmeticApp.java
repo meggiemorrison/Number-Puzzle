0c595e276313024d109ca69eebd36457a0291c08
meggiemorrison <61522124+meggiemorrison@users.noreply.github.com>
2020-07-22 08:33:46 +1200

    Update ArithmeticApp.java

--- ArithmeticApp.java
+++ ArithmeticApp.java
@@ -28,15 +28,10 @@ public class ArithmeticApp{
         System.out.println("target: " + target + ", method: " + method);
 
         if (method.equals("L")){
-            // ToDo: calculate left to right method gets called here
-           // System.out.println("@@@@@@L -> R@@@@@@@");
-           // System.out.println("Nums length " + nums.size());
-           // System.out.println("Nums zero " + nums.get(0));
             String result = Arithmetic.leftToRight(nums, "", target);
             System.out.println(result);
             
         } else if (method.equals("N")) {
-            // ToDo: calculate normal method gets called here
             System.out.println("Normal");
 
         } else {
